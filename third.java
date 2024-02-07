b4ba19f63d43bf8571a04e0e026573d5b1ede2f0
Sachin Jadhav <139002222+SACHIN9637@users.noreply.github.com>
2024-02-07 11:47:58 +0530

    Add files via upload

---
+++ third.java
@@ -0,0 +1,10 @@
+public class Third {
+    public static void main(String args[]) {
+        int i = 10;
+
+        if (i < 15)
+            System.out.println("10 is less than 15");
+
+        System.out.println("Outside if-block");
+    }
+}
