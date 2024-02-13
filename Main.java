c39ba542eee0856103d524175e3db36821dd45c4
Sachin Jadhav <139002222+SACHIN9637@users.noreply.github.com>
2024-02-13 09:29:43 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+class TArray
+{
+    public static void main(String[] args)
+    {
+        int a[][] = {{1, 2, 3}, {2, 3, 4}, {4, 5, 6}};
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
