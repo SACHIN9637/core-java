97d221a4588b037cdc270ebf16aa1515d51731f6
Sachin Jadhav <139002222+SACHIN9637@users.noreply.github.com>
2024-02-27 09:58:15 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+/*3. Write a program to print the multiplication table of a given number n.*/
+
+
+import java.util.Scanner;
+
+class MultiplicationTable
+{
+
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number for multiplication table: ");
+        int n = scanner.nextInt();
+
+        System.out.print("Enter the range up to 10 : ");
+        int range = scanner.nextInt();
+
+        scanner.close();
+
+        System.out.println("Multiplication table for " + n + ":");
+
+        for (int i = 1; i <= range; i++)
+        {
+            int result = n * i;
+            System.out.println(n + " x " + i + " = " + result);
+        }
+    }
+}
